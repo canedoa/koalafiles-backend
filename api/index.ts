be20565d3ab293eee1084f1780812a5ed6e36994
canedoa <c.canedo.amaya96@gmail.com>
2025-07-16 16:29:17 -0600

    instalacion express

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://TU_DOMINIO_FRONTEND',
-      'https://koalafiles-frontend.vercel.app',
-    ],
+    origin: ['http://localhost:4200', 'https://codewithsoul.com'],
     credentials: true,
   });
   await app.init();

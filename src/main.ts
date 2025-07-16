d3eee4b10c8296b9d3b29373c74acaf2f62a217f
canedoa <c.canedo.amaya96@gmail.com>
2025-07-16 04:11:13 -0600

    actualizaciones

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://codewithsoul.com/', 'http://localhost:4200'],
+    origin: ['https://codewithsoul.com', 'http://localhost:4200'],
 
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }

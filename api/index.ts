a5cd5d43d39aad9fe77a84c80f35faec7d6cac31
canedoa <c.canedo.amaya96@gmail.com>
2025-07-16 16:12:49 -0600

    modificacion index

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,14 @@ const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://TU_DOMINIO_FRONTEND',
+      'https://koalafiles-frontend.vercel.app',
+    ],
+    credentials: true,
+  });
   await app.init();
 }
 

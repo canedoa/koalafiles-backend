a892dd3ea265ba4647ecfc41175fab56ba45bcb4
canedoa <c.canedo.amaya96@gmail.com>
2025-07-16 18:34:57 -0600

    modificacion origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['https://codewithsoul.com', 'http://localhost:4200'],
 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
